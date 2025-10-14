ee0a0a0c618fdfb0db9a6f4449c3e0dd419ee5a1
Nickah <andrianarisoa3544@gmail.com>
2025-10-14 14:00:26 +0300

    auto

--- backend/src/main/java/com/example/films/repository/SceneTournageRepository.java
+++ backend/src/main/java/com/example/films/repository/SceneTournageRepository.java
@@ -37,6 +37,7 @@ public interface SceneTournageRepository extends JpaRepository<SceneTournage, Lo
     
     @Query("SELECT st FROM SceneTournage st LEFT JOIN FETCH st.scene s LEFT JOIN FETCH s.sequence seq LEFT JOIN FETCH seq.episode e LEFT JOIN FETCH e.projet WHERE st.id = :id")
     Optional<SceneTournage> findByIdWithDetails(@Param("id") Long id);
+   
 
     // @Query("SELECT st FROM SceneTournage st " +
     //        "JOIN st.scene s " +
