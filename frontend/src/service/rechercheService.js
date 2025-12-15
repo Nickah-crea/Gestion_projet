7be60c16f285b36c66b51f512d0314796af53e2f
Nickah-crea <andrianarisoa3544@gmail.com>
2025-12-15 11:16:25 +0100

    all

--- frontend/src/service/rechercheService.js
+++ frontend/src/service/rechercheService.js
@@ -1,7 +1,7 @@
 // src/services/rechercheService.js
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8080'
+const API_BASE_URL = 'http://localhost:8080/api'
 
 export const rechercheAvancee = async (criteres) => {
   try {
