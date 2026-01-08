655f9efdfd06f539bbcb2e30f816fe0438b0754c
Nickah <andrianarisoa3544@gmail.com>
2026-01-08 16:33:07 +0300

    projet

--- frontend/src/views/ProjetScenariste.vue
+++ frontend/src/views/ProjetScenariste.vue
@@ -182,9 +182,9 @@
               <!-- Actions en bas de carte -->
               <div class="movie-actions-bottom-Scenariste">
                 <div class="actions-top-Scenariste">
-                  <button class="action-btn-Scenariste accent-btn" @click="goToDetails(episode.idEpisode)" title="Détails">
-                    <i class="fas fa-info-circle icon"></i>
-                    <span>Détails</span>
+                  <button class="action-btn-Scenariste primary-btn" @click="goToDetails(episode.idEpisode)" title="Détails">
+                    <i class="fa fa-film"></i>
+                    <span>Aller dans Séquences</span>
                   </button>
                 </div>
               </div>
